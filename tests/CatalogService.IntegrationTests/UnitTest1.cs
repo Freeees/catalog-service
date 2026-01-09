797ac3609cf328a1755a944ab4460524acf3bec7
frees <gredindn@gmail.com>
2026-01-08 16:28:42 -0800

    Добавьте файлы проекта.

---
+++ tests/CatalogService.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CatalogService.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/CatalogService.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CatalogService.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
